d0d1312a14937284bf427ff22ecae4f550a591fe
STELSv1 <147424432+STELSv1@users.noreply.github.com>
2026-05-08 11:00:00 +0300

    feat: add app entry point and root App component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BookingProvider } from "./context/BookingContext";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+
+export default function App() {
+  return (
+    <BookingProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking/:trainId" element={<Booking />} />
+        </Routes>
+        <ToastContainer position="top-center" />
+      </BrowserRouter>
+    </BookingProvider>
+  );
+}
